47e78643982ec8adfd65faf20364c82c86a5e04b
Maberthaab <thataaberlian@gmail.com>
2023-06-10 00:15:08 +0700

    update app.jsx & index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import Header from './components/header/Header'
+import Navbar from './components/nav/Navbar'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Portfolio from './components/portfolio/Portfolio'
+import Contact from './components/contact/Contact'
+
 
 const App = () => {
   return (
     <>
-        <Headers
+        <Headers />
+        <Nav />
+        <About />
+        <Experience />
+        <Portfolio />
+        <Contact />
+        <Footer />
+
+    
     
     </>
   )
