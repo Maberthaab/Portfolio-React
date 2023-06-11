80051fd58ed28d1554386d22d36c4437fcb3a140
Maberthaab <thataaberlian@gmail.com>
2023-06-11 12:40:24 +0700

    About Component JSX

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.jpg'
+import {FaAward} from 'react-icons/fa'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <section id='about'> About </section>
+    <section id='about'> 
+    <h5> Get To Know </h5>
+    <h2> About Me</h2>
+
+    <div className="container about__container">
+      <div className="about__me">
+        <div className="about__me-image">
+          <img src={ME} alt="Image" />
+        </div>
+      </div>
+
+      <div className="about__content">
+      <div className="about__cards">
+        <article className='about__card'>
+          <FaAward className='about__icon' />
+          <h5> Experience </h5>
+          <small> Junior Experience</small>
+        </article>
+
+
+        <article className='about__card'>
+          <VscFolderLibrary className='about__icon' />
+          <h5> Project </h5>
+          <small> My Project Completed</small>
+        </article>
+      </div>
+
+      <p> 
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, odit.
+      </p>
+
+      <a href="#contact" className='btn btn-primary'> Let's Talk</a>
+      </div>
+    </div>
+
+    </section>
   )
 }
 
