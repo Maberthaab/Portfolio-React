d3b576eb424b36a804bcf40333da3101b9a319d6
Maberthaab <thataaberlian@gmail.com>
2023-06-11 11:29:28 +0700

    membuat header

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import Header from './components/header/Header'
-import Navbar from './components/nav/Navbar'
+import Navbar from './components/navbar/Navbar'
 import About from './components/about/About'
 import Experience from './components/experience/Experience'
 import Portfolio from './components/portfolio/Portfolio'
 import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
 
 
 const App = () => {
   return (
     <>
-        <Headers />
-        <Nav />
+        <Header />
+        <Navbar />
         <About />
         <Experience />
         <Portfolio />
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './navbar.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BiBook} from 'react-icons/ai'
 
 const navbar = () => {
   return (
-    <div>navbar</div>
+    <nav>
+        <a href='#'><AiOutlineHome /></a>
+        <a href='#about'><AiOutlineUser /></a>
+        <a href='#experience'><BiBook /></a>
+        <a href='#contact'><AiOutlineHome /></a>
+      </nav>
   )
 }
 
