cc00ff9b2b4846115a617c453d21895d9698c7e2
Maberthaab <thataaberlian@gmail.com>
2023-06-20 00:45:28 +0700

    portfolio jsx

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,22 @@ import './portfolio.css'
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <section id='portfolio'>
+      <h5> My Project </h5>
+      <h2> Portfolio </h2>
+
+      <div className='container portfolio__container'>
+        <article>
+          <div className="portfolio__item">
+            <div className="portfolio__item-image">
+            <h3> This is a Portfolio Item</h3>
+            <a href ="#" className='btn'> </a>
+
+            </div>
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
