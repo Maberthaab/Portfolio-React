c0e0dfbbe73dda3fd5aac2ca48777856e62d6705
Maberthaab <thataaberlian@gmail.com>
2023-06-27 13:40:52 +0700

    footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import './footer.css'
-
+import {FiInstagram} from 'react-icons/fi'
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href='#' className='footer__logo'>Mabertha</a>
+
+      <ul className='links'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#experience'>Experience</a></li>
+        <li><a href='#portfolio'>Portfolio</a></li>
+        <li><a href='#certificate'>Certificate</a></li>
+        <li><a href='#contact'>Contact</a></li>
+      </ul>
+
+      <div className='footer__socials'>
+      <a href='https://linkedin.com/in/mabertha.berliana'></a>
+        <a href='https://instagram.com/mabertha.ab'><FiInstagram /></a>
+        <a href='https://github.com/Maberthaab'></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Portfolio Me </small>
+
+      </div>
+    </footer>
   )
 }
 
