ff94b9492a43670f27ea4cefa05352c4fc36f2a6
Maberthaab <thataaberlian@gmail.com>
2023-07-01 22:33:19 +0700

    download cv

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import CV from '../../assets/cvtha.pdf'
+import cvtha from '../../assets/cvtha.pdf'
 
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={CV} download className='btn'>Download CV</a>
+        <a href={cvtha} download className='btn'>Download CV</a>
         <a href="#contact" className='btn btn-primary'> Let's Talk</a>
     </div>
   )
